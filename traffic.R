8707858dee10887211d3d283a3f7793eab2cf59f
Pht27 <phili2709@gmail.com>
2021-07-09 09:39:28 +0200

    ÖPNV und Unfälle hinzugefügt

---
+++ traffic.R
@@ -0,0 +1,27 @@
+source("incidence_lk.R")
+
+unfaelle_data <- read_csv("csvs/unfaelle_jahre.csv")
+
+unfaelle_data %>% mutate(Jahr=as.character(Jahr))
+
+ggplot(data=unfaelle_data, aes(x=Monat, y=Unfaelle, fill=factor(Jahr))) +
+  geom_col(position=position_dodge()) + scale_x_discrete(limits=unfaelle_data$Monat[1:12]) +
+  labs(fill = "Jahr") -> unfaelle_plt
+
+sti_germany <- calc_sti_germany()
+tibble(date=days_since_2020[1:366], sti=sti_germany[1:366], Landkreis="GER") -> df
+ggplot(data=df, aes(x=date, y=sti, color=Landkreis)) + geom_line() -> plt_germany
+
+
+
+
+oepnv_data <- read_csv("csvs/oepnv_jahre.csv")
+
+oepnv_data %>% filter(Bundesland=="Hessen") -> oepnv_data
+
+ggplot(data=oepnv_data, aes(x=Quartal, y=Personenkilometer, fill=factor(Jahr))) +
+  geom_col(position=position_dodge()) + scale_x_discrete(limits=oepnv_data$Quartal[1:4]) +
+  labs(fill = "Jahr") -> oepnv_plt
+
+
+cowplot::plot_grid(plotlist = list(unfaelle_plt, oepnv_plt), nrow=2) %>% print()
