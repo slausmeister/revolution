ba8cf11a5fb82b98f79b1792a2c126191d597063
slaus <stanislaussteinvk@gmail.cm>
2021-08-08 15:27:15 +0200

    added sti.R and dependancies

--- R/read_data.R
+++ R/read_data.R
@@ -1,20 +1,49 @@
+# This is the main data preperation function.
 #' @export
-#' @importFrom magrittr %>%
 read_rki_data <- function(){
 # import the population of 'Landkreis' with the given csv
 population_lk_data <- readr::read_csv(system.file("extdata", "population_lk.csv", package="revolution"))
 
 # transform the 'IdLandkreis' column to a numeric
-population_lk_data %>% dplyr::mutate(IdLandkreis=as.numeric(IdLandkreis)) ->> population_lk_data
+population_lk_data %>% dplyr::mutate(IdLandkreis=as.numeric(IdLandkreis)) -> population_lk_data
 
 # calculate the total german population
 population_lk_data %>% dplyr::summarise(n=sum(Bevölkerung)) %>%
-  `[[`(1) ->> total_population_germany
+  `[[`(1) -> total_population_germany
 
 # import the population per age group in 2020
 readr::read_csv(system.file("extdata", "population_age.csv", package="revolution")) %>% dplyr::filter(Jahr==2020) %>%
-  dplyr::group_by(Altersgruppe) %>% dplyr::summarise(Bevölkerung=sum(Bevölkerung)) ->>
+  dplyr::group_by(Altersgruppe) %>% dplyr::summarise(Bevölkerung=sum(Bevölkerung)) ->
   population_age_2020_data
+
+### rki covid data:
+# import raw rki data
+rki_data <- readr::read_csv(system.file("extdata", "RKI_COVID19.csv", package="revolution"))
+
+# the 'Neuer' and 'Datenstand' columns compare this dataset to the one from yesterday,
+# which makes it useless for our research
+rki_data %>% dplyr::select(-NeuerFall, -NeuerTodesfall, -NeuGenesen, -Datenstand) ->
+  rki_data
+
+# change the column type of 'Meldedatum' and 'Refdatum' to date
+rki_data %>% dplyr::mutate(Meldedatum=as.Date(Meldedatum), Refdatum=as.Date(Refdatum)) -> rki_data
+
+# in most cases, 'Altersgruppe2' is not available
+rki_data %>% dplyr::select(-Altersgruppe2) -> rki_data
+
+# transform the 'IdLandkreis' column to a numeric
+rki_data %>% dplyr::mutate(IdLandkreis=as.numeric(IdLandkreis)) -> rki_data
+
+# we will use the 'Landkreis' column from the other csv, because of normalization
+rki_data %>% dplyr::select(-Landkreis) %>%
+  dplyr::left_join(dplyr::select(readr::read_csv(system.file("extdata","population_lk.csv", package="revolution")), IdLandkreis, Landkreis), by="IdLandkreis") ->
+  rki_data
+
+# 'IdBundesland' is a part of 'IdLandkreis' and we have the 'Bundesland' column
+rki_data %>% dplyr::select(-IdBundesland) -> rki_data
+
+# 'FID' is the case id, which is useless for our research
+rki_data %>% dplyr::select(-FID) -> rki_data
+
+return(rki_data)
 }
-
-
---
+++ R/utilities.R
@@ -0,0 +1,90 @@
+# All these funtions are used for data preperation. They do not need to be user exposed
+#' @importFrom magrittr %>%
+
+get_bundesland_id_from_lk_id <- function(lk_id){
+  return((lk_id - lk_id %% 1000) / 1000)
+}
+
+# get the LandkreisID from a input string
+
+get_lk_id_from_string <- function(lk_name, print_process=F){
+  population_lk_data %>% dplyr::filter(stringr::str_detect(Landkreis, regex(lk_name, ignore_case=T))) %>%
+     `[[`("Landkreis") -> lks
+
+  population_lk_data %>% dplyr::filter(stringr::str_detect(Landkreis, regex(lk_name, ignore_case=T))) %>%
+    `[[`("IdLandkreis") -> lk_ids
+
+  if(length(lks) == 1) return(lk_ids)
+  if(length(lks) == 0) {
+    cat("No match found for:\n")
+    print(lk_name)
+    cat("Defaulting to Heidelberg\n")
+    return(8221)
+  }
+
+  if(print_process){
+    cat("For ", lk_name, " the following 'Landkreise' were found\n")
+    print(lks)
+  }
+
+  for(i in 1:length(lks)) if(tolower(lks[i]) == tolower(lk_name)){
+    if(print_process){
+      cat("Because of an exact match, the following 'Landkreis' was returned:\n")
+      print(lk_name)
+      cat("If this is wrong, please type the exact 'Landkreis'\n")
+    }
+    return(lk_ids[i])
+  }
+
+  if(print_process){
+    cat("The following 'Landkreis' was returned:\n")
+    print(lk_name)
+    cat("If this is wrong, please type the exact 'Landkreis'\n")
+  }
+
+    return(lk_ids[[1]])
+}
+
+
+get_age_label_from_number <- function(age_number){
+  if(as.integer(age_number) < 0) return("A00-A04")
+  if(as.integer(age_number) < 5) return("A00-A04")
+  if(as.integer(age_number) < 15) return("A05-A14")
+  if(as.integer(age_number) < 34) return("A15-A34")
+  if(as.integer(age_number) < 59) return("A35-A59")
+  if(as.integer(age_number) < 79) return("A60-A79")
+  return("A80+")
+}
+
+filter_data_by <- function(ages="all", regions="Germany", from="2020-01-01", to=Sys.Date()){
+  data <- rki_data
+  # filter the age groups
+  if(!all(ages=="all")){
+    for(i in 1:length(ages)){
+      ages[i] <- get_age_label_from_number(ages[i])
+    }
+    data %>% dplyr::filter(Altersgruppe %in% ages) -> data
+  }
+
+  # filter the time span
+  data %>% dplyr::filter(Meldedatum >= from, Meldedatum <= to) -> data
+
+  # filter the regions (not robust at the moment)
+  rki_data %>% dplyr::select(Bundesland) %>% unique() %>%
+    dplyr::filter(!Bundesland %in% c("Berlin", "Bremen", "Hamburg")) %>%
+    `[[`("Bundesland") %>% tolower() -> bundesländer
+
+  if(all(tolower(regions) %in% bundesländer)){
+    data %>% dplyr::filter(tolower(Bundesland) %in% tolower(regions)) -> data
+  }
+  else if(!all(tolower(regions)=="germany")){
+    for(i in 1:length(regions)) regions[i] <- get_lk_id_from_string(regions[i], print_process=F)
+    data %>% dplyr::filter(IdLandkreis %in% regions) -> data
+  }
+
+  return(data)
+}
+
+days_since_2020 <- function(){
+    return(seq(as.Date("2020-01-01"), as.Date(Sys.Date()), by="days"))
+}
