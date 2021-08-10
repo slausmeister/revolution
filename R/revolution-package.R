e2f900816c05a66901c626a1ee20f478616aaddb
Pht27 <phili2709@gmail.com>
2021-08-10 17:10:12 +0200

    description und paket doc hinzugefügt

---
+++ R/revolution-package.R
@@ -0,0 +1,21 @@
+#' @details
+#' Frag mich nicht nach Details
+#' @section Hallelujah:
+#' Hoffentlich kriegen wir eine <3.0!
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#' @keywords internal
+
+"_PACKAGE"
